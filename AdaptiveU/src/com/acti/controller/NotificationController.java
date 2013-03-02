899ede4ee517ae1d990e852adbf8cc79e282e382
Ananthakrishnan <Ananthakrishnan@10.4.2.70>
2013-03-02 18:26:01 +0530

    @author a3v for commit notification changes

--- AdaptiveU/src/com/acti/controller/NotificationController.java
+++ AdaptiveU/src/com/acti/controller/NotificationController.java
@@ -1,11 +1,13 @@
 package com.acti.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.adaptive.business.service.NotificationService;
 
+@Controller
 public class NotificationController
 	{
 		@RequestMapping( value = "/pushNotification", method = RequestMethod.POST )
